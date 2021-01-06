93ae611e1f82b74a0a1ff75fbd1b3913a8ad518d
Khaledmarcello <66645843+Khaledmarcello@users.noreply.github.com>
2021-01-06 04:52:24 +0200

    Modification in paths

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String args[]) {
         Huffman h=new Huffman();
-        h.Compress("C:\\Documents\\GitHub\\StandardHuffman\\test.txt", "compressed");
-        h.Decompress("C:\\Documents\\GitHub\\StandardHuffman\\compressed.txt", "decompressed");
+        h.Compress("C:\\Users\\RCSC\\Documents\\GitHub\\StandardHuffman\\test.txt", "compressed");
+        h.Decompress("C:\\Users\\RCSC\\Documents\\GitHub\\StandardHuffman\\compressed.txt", "decompressed");
     }
 }
